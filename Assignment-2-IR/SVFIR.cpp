81faa050fb43676b7eaf5fa1493a7d3f9f3fed5f
yangzehao <2785602345@qq.com>
2025-10-14 18:51:33 +0800

    homework

--- Assignment-2-IR/SVFIR.cpp
+++ Assignment-2-IR/SVFIR.cpp
@@ -13,42 +13,33 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    int arg_num = 0;
-    int extraArgc = 4;
-    char** arg_value = new char*[argc + extraArgc];
-    for (; arg_num < argc; ++arg_num) {
-        arg_value[arg_num] = argv[arg_num];
-    }
-    std::vector<std::string> moduleNameVec;
+    int arg_num = 0;
+    int extraArgc = 4;
+    char** arg_value = new char*[argc + extraArgc];
+    for (; arg_num < argc; ++arg_num) {
+        arg_value[arg_num] = argv[arg_num];
+    }
+    std::vector<std::string> moduleNameVec;
+    int orgArgNum = arg_num;
+    arg_value[arg_num++] = (char*)"-model-arrays=true";
+    arg_value[arg_num++] = (char*)"-pre-field-sensitive=false";
+    arg_value[arg_num++] = (char*)"-model-consts=true";
+    arg_value[arg_num++] = (char*)"-stat=false";
+    assert(arg_num == (orgArgNum + extraArgc) && "more extra arguments? Change the value of extraArgc");
 
-    int orgArgNum = arg_num;
-    arg_value[arg_num++] = (char*)"-model-arrays=true";
-    arg_value[arg_num++] = (char*)"-pre-field-sensitive=false";
-    arg_value[arg_num++] = (char*)"-model-consts=true";
-    arg_value[arg_num++] = (char*)"-stat=false";
-    assert(arg_num == (orgArgNum + extraArgc) && "more extra arguments? Change the value of extraArgc");
-
-    moduleNameVec = OptionBase::parseOptions(arg_num, arg_value, "SVF IR", "[options] <input-bitcode...>");
-
-    LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
-
-    // Instantiate an SVFIR builder
-    SVFIRBuilder builder;
-    cout << "Generating SVFIR(PAG), call graph and ICFG ..." << endl;
-
-    // TODO: here, generate SVFIR(PAG), call graph and ICFG, and dump them to files
-    //@{
-
-    SVFIR* pag = builder.build();
-    CallGraph* cg = pag->getCallGraph();
-    ICFG* icgf = pag->getICFG();
-    pag->dump();
-    cg->dump();
-    icgf->dump();
-
-    //@}
+    moduleNameVec = OptionBase::parseOptions(arg_num, arg_value, "SVF IR", "[options] <input-bitcode...>");
 
 
+    LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
 
-    return 0;
+    SVFIRBuilder builder;
+    cout << "Generating SVFIR(PAG), call graph and ICFG ..." << endl;
+
+    SVFIR* pag = builder.build();
+    CallGraph* cg = pag->getCallGraph();
+    ICFG* icgf = pag->getICFG();
+    pag->dump();
+    cg->dump();
+    icgf->dump();
+    return 0;
 }
